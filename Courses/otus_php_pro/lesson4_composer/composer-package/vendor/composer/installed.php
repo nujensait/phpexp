b3d34e291919b036c825f94bbfecd6312875058d
mishaikon@gmail.com <7tiou3tlet>
2022-02-24 16:24:17 +0300

    Otus PHP pro, hw4

---
+++ Courses/otus_php_pro/lesson4_composer/composer-package/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6043ee25f19cde93d758081571a8c0bab12e3afc',
+    'name' => 'nujensait/translit',
+  ),
+  'versions' => 
+  array (
+    'nujensait/translit' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6043ee25f19cde93d758081571a8c0bab12e3afc',
+    ),
+  ),
+);
