359627bd1e5e88a345b9a37242f4b93ee3d5c246
mishaikon@gmail.com <7tiou3tlet>
2023-05-22 12:55:52 +0300

    Robusta interview, task 1: matrix rank (my oop solution)

--- Interview/Robusta/matrix_rank.php
+++ Interview/Robusta/matrix_rank.php
@@ -4,38 +4,53 @@
  * Программа для вычисления ранга матрицы из вещественных чисел
  */
 
+/**
+ * Интерфейс для скалярных операцийнад матрицами
+ */
 interface iScalarMatrixOperation
 {
     public function init(array $matrix): void;
     public function calc(): float;
 }
 
+/**
+ * Операцуия по вычсилению ранга матрицы
+ */
 class MatrixRankOperation implements iScalarMatrixOperation
 {
     private array $matrix;
-
-    public function init(array $matrix): void {
-        $this->matrix = $matrix;
-    }
+    private int $rowsCnt;
+    private int $columnsCnt;
 
     /**
-     * Рангом системы строк (столбцов) матрицы A с m строками и n столбцами называется максимальное число линейно независимых строк (столбцов).
-     * Несколько строк (столбцов) называются линейно независимыми, если ни одна из них не выражается линейно через другие.
-     * Ранг системы строк всегда равен рангу системы столбцов, и это число называется рангом матрицы.
+     * Инициализация
+     * @param array $matrix
      * @return void
      */
-    public function calc(): float
-    {
+    public function init(array $matrix): void {
+        $this->matrix = $matrix;
         // Получаем количество строк и столбцов матрицы
         $rows = count($this->matrix);
         $columns = count($this->matrix[0]);
+    }
 
+    /**
+     * Вычисляем ранг матрицы
+     *
+     * Рангом системы строк (столбцов) матрицы A с M строками и N столбцами называется максимальное число линейно независимых строк (столбцов).
+     * Несколько строк (столбцов) называются линейно независимыми, если ни одна из них не выражается линейно через другие.
+     * Ранг системы строк всегда равен рангу системы столбцов, и это число называется рангом матрицы.
+     *
+     * @return float
+     */
+    public function calc(): float
+    {
         // Создаем массив для хранения уникальных элементов
         $uniqueElements = array();
 
         // Проходим по всем элементам матрицы и добавляем уникальные элементы в массив
-        for ($i = 0; $i < $rows; $i++) {
-            for ($j = 0; $j < $columns; $j++) {
+        for ($i = 0; $i < $this->rowsCnt; $i++) {
+            for ($j = 0; $j < $this->columnsCnt; $j++) {
                 if (!in_array($this->matrix[$i][$j], $uniqueElements)) {
                     $uniqueElements[] = $this->matrix[$i][$j];
                 }
@@ -45,21 +60,18 @@ class MatrixRankOperation implements iScalarMatrixOperation
         // Сортируем массив уникальных элементов по возрастанию
         sort($uniqueElements);
 
-        // Получаем количество уникальных элементов
-        $uniqueCount = count($uniqueElements);
-
         // Вычисляем ранг матрицы
-        $rank = $columns;
+        $rank = $this->columnsCnt;
 
-        for ($i = 0; $i < $rows; $i++) {
+        for ($i = 0; $i < $this->rowsCnt; $i++) {
             $rowElements = array();
-            for ($j = 0; $j < $columns; $j++) {
+            for ($j = 0; $j < $this->columnsCnt; $j++) {
                 if (!in_array($this->matrix[$i][$j], $rowElements)) {
                     $rowElements[] = $this->matrix[$i][$j];
                 }
             }
             $rowUniqueCount = count($rowElements);
-            if ($rowUniqueCount < $columns) {
+            if ($rowUniqueCount < $this->columnsCnt) {
                 $rank--;
             }
         }
