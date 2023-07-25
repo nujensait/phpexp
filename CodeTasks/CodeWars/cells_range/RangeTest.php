3a1b24c7daab2e66b16d69aead2226fb82529cfd
mishaikon <mishaikon@gmail.com>
2023-07-25 17:18:48 +0300

    CodeWars Excel cells range task + testCase

---
+++ CodeTasks/CodeWars/cells_range/RangeTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Test range.php script
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class RangeTest extends TestCase
+{
+    public function basicTestsProvider()
+    {
+        return [
+            'Range A1:A10' => ['A1:A10', [
+                'A1', 'A2', 'A3', 'A4', 'A5',
+                'A6', 'A7', 'A8', 'A9', 'A10'
+            ]],
+            'Range A1:Z1' => ['A1:Z1', [
+                'A1', 'B1', 'C1', 'D1', 'E1',
+                'F1', 'G1', 'H1', 'I1', 'J1',
+                'K1', 'L1', 'M1', 'N1', 'O1',
+                'P1', 'Q1', 'R1', 'S1', 'T1',
+                'U1', 'V1', 'W1', 'X1', 'Y1',
+                'Z1'
+            ]],
+            'Range F12:J17' => ['F12:J17', [
+                'F12', 'G12', 'H12', 'I12',
+                'J12', 'F13', 'G13', 'H13',
+                'I13', 'J13', 'F14', 'G14',
+                'H14', 'I14', 'J14', 'F15',
+                'G15', 'H15', 'I15', 'J15',
+                'F16', 'G16', 'H16', 'I16',
+                'J16', 'F17', 'G17', 'H17',
+                'I17', 'J17'
+            ]],
+            'Range B3:D5' => ['B3:D5', [
+                'B3', 'C3', 'D3',
+                'B4', 'C4', 'D4',
+                'B5', 'C5', 'D5'
+            ]],
+            'Range A1:B2' => ['A1:B2', ['A1', 'B1', 'A2', 'B2']],
+            'Range W118:Z124' => ['W118:Z124', [
+                'W118', 'X118', 'Y118', 'Z118',
+                'W119', 'X119', 'Y119', 'Z119',
+                'W120', 'X120', 'Y120', 'Z120',
+                'W121', 'X121', 'Y121', 'Z121',
+                'W122', 'X122', 'Y122', 'Z122',
+                'W123', 'X123', 'Y123', 'Z123',
+                'W124', 'X124', 'Y124', 'Z124'
+            ]],
+            'Range H7:F3 (invalid)' => ['H7:F3', []],
+            'Range C2:C2 (invalid)' => ['C2:C2', []],
+        ];
+    }
+    /**
+     * @dataProvider basicTestsProvider
+     */
+    public function testBasic(string $input, array $expected)
+    {
+        $this->assertEquals($expected, getCellAddresses($input));
+    }
+}
--- CodeTasks/CodeWars/cells_range/range.php
+++ CodeTasks/CodeWars/cells_range/range.php
@@ -1,34 +1,111 @@
 <?php
 
 /**
- * Get cells addresses
- * @param $range
- * @return array
+ * Excel cells range
+ * @see https://www.codewars.com/kata/62c376ce1019024820580309/train/php
+ * @see Readme.md
+ *
+ * @fixme: make chg to use cells addr like AA1 ... ZZ3
  */
-function getCellAddresses($range)
+
+class ExcelSheet
 {
-    if (!preg_match('/^([A-Z]+)(\d+):([A-Z]+)(\d+)$/', $range, $matches)) {
-        return [];
+    public function getCellAddresses(string $range): ?array
+    {
+        $matrix = $this->getCellAddressesMatrix($range);
+        if($matrix == null) {
+            return null;
+        }
+
+        $this->printCellsMatrix($matrix);
+
+        $res = [];
+        foreach($matrix as $row) {
+            foreach($row as $cell) {
+                $res[] = $cell;
+            }
+        }
+
+        return $res;
     }
 
-    list($_, $startCol, $startRow, $endCol, $endRow) = $matches;
+    /**
+     * Get cells addresses
+     * @param $range
+     * @return array
+     */
+    public function getCellAddressesMatrix(string $range): ?array
+    {
+        $range = $this->getRange($range);
+        var_dump($range);
+        if ($range == null) {
+            return null;
+        }
+        list($startCol, $startRow, $endCol, $endRow) = $range;
 
-    if (ord($startCol) > ord($endCol) || $startRow > $endRow) {
-        return [];
-    }
+        $result = [];
 
-    $result = [];
-
-    for ($col = $startCol; $col <= $endCol; $col++) {
         for ($row = $startRow; $row <= $endRow; $row++) {
-            $result[] = $col . $row;
+            for ($col = $startCol; $col <= $endCol; $col++) {
+                $result[$row - 1][] = $col . $row;
+            }
+        }
+
+        //var_dump($result); die();
+        //sort($result);
+
+        return $result;
+    }
+
+    /**
+     * Get cells range as array
+     * @param string $range
+     * @return array
+     */
+    public function getRange(string $range): array
+    {
+        if (!preg_match('/^([A-Z]+)(\d+):([A-Z]+)(\d+)$/', $range, $matches)) {
+            return [];
+        }
+
+        list($_, $startCol, $startRow, $endCol, $endRow) = $matches;
+
+        if (ord($startCol) > ord($endCol) || $startRow > $endRow) {
+            return [];
+        }
+
+        return [(string)$startCol, (int)$startRow, (string)$endCol, (int)$endRow];
+    }
+
+    /**
+     * @param $arr
+     * @return void
+     */
+    public function printCellsMatrix(array $arr): void
+    {
+        for ($i = 0; $i < count($arr); $i++) {
+            for ($j = 0; $j < count($arr[$i]); $j++) {
+                print $arr[$i][$j] . " ";
+            }
+            echo "\n";
         }
     }
-
-    sort($result);
-
-    return $result;
-
 }
 
-var_dump(getCellAddresses("B1:H5"));
+/**
+ * @param string $range
+ * @return array
+ */
+function getCellAddresses(string $range): array
+{
+    $sheet = new ExcelSheet();
+    $cells = $sheet->getCellAddresses($range);
+    //$sheet->printCells($cells);
+    return $cells;
+}
+
+//$cells = getCellAddresses("B1:H5");
+//var_dump($cells);
+
+$cells = getCellAddresses("A1:A12");
+var_dump($cells);
